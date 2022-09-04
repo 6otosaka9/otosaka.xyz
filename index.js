f35a704d3583dda87fa6ad6584545feed85a28f5
6otosaka9 <otosakayu460@gmail.com>
2022-09-03 22:20:37 -0500

    update

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 _path = process.cwd();
 const express = require("express");
+const cors = require("cors");
+const secure = require("ssl-express-www");
+const morgan = require("morgan");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000 || 8080 || 4000;
 
 app.use(express.static(_path + "/public"));
-app.use("/api", require(_path + "./src/routes/api.js"))
-app.use("/", require(_path + "./src/routes/pages.js"))
+app.use(cors());
+app.use(secure);
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.use("/api", require(_path + "/src/routes/api.js"))
+app.use("/", require(_path + "/src/routes/pages.js"))
 
 
 app.listen(port, () => {
